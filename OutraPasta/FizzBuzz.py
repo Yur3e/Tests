e408abbf1579e187c9b155a70e3b0bea1b45a283
Yure Fernandes <joseyure42@gmail.com>
2022-07-30 00:17:41 -0300

    Create other foulder

---
+++ OutraPasta/FizzBuzz.py
@@ -0,0 +1,12 @@
+def fb(n):
+    for n in range(1, n+1):
+        if n%3==0 and n%5==0:
+            print('FizzBuzz')
+        elif n%3==0:
+            print('Fizz')
+        elif n%5==0:
+            print('Buzz')
+        else:
+            print(n)
+
+fb(100)
